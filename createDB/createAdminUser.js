735790d7bd9c4a33c0007daf5ae7027f3d7e9d35
VengLeapChan <leapvchan@gmail.com>
2024-02-05 23:06:31 -0800

    added post friend request route

--- createDB/createAdminUser.js
+++
@@ -1,9 +0,0 @@
-// get the admin db
-db = db.getSiblingDB("admin");
-
-// create an admin user
-db.createUser({
-  user: "dbAdmin",
-  pwd: "test",
-  roles: ["readWriteAnyDatabase", "dbAdminAnyDatabase", "clusterAdmin"],
-});
--- interfaces/IGroupModel.ts
+++
@@ -1,9 +0,0 @@
-import Mongoose from "mongoose";
-
-interface IGroupModel extends Mongoose.Document {
-  groupID: string,
-  groupName: string,
-  usersInGroup: [{userID: string}],
-}
-
-export {IGroupModel};
