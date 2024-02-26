efe29cfee3f523c9b49d4057d5679c873cc55c9c
VengLeapChan <leapvchan@gmail.com>
2024-02-25 23:51:47 -0800

    message

--- interfaces/IGroupModel.ts
+++
@@ -1,9 +0,0 @@
-import Mongoose from "mongoose";
-
-interface IGroupModel extends Mongoose.Document {
-  groupID: string,
-  groupName: string,
-  usersInGroup: [string],
-}
-
-export {IGroupModel};
--- interfaces/IReceiptSplitModel.ts
+++
@@ -1,9 +0,0 @@
-import Mongoose from "mongoose";
-
-interface IReceiptSplitModel extends Mongoose.Document {
-  receiptSplitID: string,
-  receiptSplitAmount: number,
-  receiptTargetID: string ,
-}
-
-export { IReceiptSplitModel };
