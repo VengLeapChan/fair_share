e61563977abffad332a00d0faeb8a45218e26ee4
VengLeapChan <leapvchan@gmail.com>
2024-03-10 00:19:40 -0800

    commit

--- App.js
+++ App.js
@@ -75,6 +75,7 @@ class App {
         });
         router.get('/app/logout', this.validateAuth, (req, res) => {
             req.logout((err) => {
+                console.log("Logging Out User");
                 if (err) {
                     console.error(err);
                     res.status(500).send('Error logging out');
@@ -121,7 +122,6 @@ class App {
             const userObject = JSON.parse(profile);
             const userId = userObject.id;
             const receiptID = req.params.receiptID;
-            console.log("getting receipt: ", receiptID, " from user: ", userId);
             try {
                 const receipt = yield this.Receipt.getSpecificReceipt(res, userId, receiptID);
                 if (receipt) {
@@ -161,7 +161,6 @@ class App {
             const profile = JSON.stringify(req.user);
             const userObject = JSON.parse(profile);
             const userID = userObject.id;
-            console.log("userID", userID);
             var receiptObject = req.body;
             receiptObject.receiptID = newReceiptId;
             try {
@@ -173,7 +172,7 @@ class App {
             }
         }));
         // delete A specific receipt 
-        router.delete("/app/receipt/:receiptID", (req, res) => __awaiter(this, void 0, void 0, function* () {
+        router.delete("/app/receipt/:receiptID", this.validateAuth, (req, res) => __awaiter(this, void 0, void 0, function* () {
             const profile = JSON.stringify(req.user);
             const userObject = JSON.parse(profile);
             const userId = userObject.id;
@@ -187,7 +186,7 @@ class App {
             }
         }));
         // add receipt item
-        router.post('/app/receipt/:receiptID/receiptItem', (req, res) => __awaiter(this, void 0, void 0, function* () {
+        router.post('/app/receipt/:receiptID/receiptItem', this.validateAuth, (req, res) => __awaiter(this, void 0, void 0, function* () {
             const newReceiptItemId = crypto.randomBytes(16).toString("hex");
             const receiptID = req.params.receiptID;
             const profile = JSON.stringify(req.user);
@@ -205,77 +204,127 @@ class App {
                 console.error(e);
             }
         }));
-        // ROUTES FOR USER
-        // router.get("/app/user", async (req, res) => {
-        //   console.log('Query All User');
-        //   await this.User.retreiveAllUsers(res);
-        // })
-        router.get("/app/user/:id", (req, res) => __awaiter(this, void 0, void 0, function* () {
-            console.log("Query Single User");
-            const id = req.params.id;
-            yield this.User.retreiveSpecificUser(res, id);
-        }));
-        router.post('/app/user/', (req, res) => __awaiter(this, void 0, void 0, function* () {
-            console.log("Adding a user");
-            const id = crypto.randomBytes(16).toString("hex");
-            console.log(req.body);
-            var jsonObj = req.body;
-            jsonObj.userID = id;
-            const doc = new this.User.model(jsonObj);
+        //Unprotected Routes
+        //routes to go get a list of receipts
+        router.get('/app/:userID/receipt', (req, res) => __awaiter(this, void 0, void 0, function* () {
+            const userId = req.params.userID;
             try {
-                yield doc.save();
-                res.send('{"id":"' + id + '"}');
+                yield this.Receipt.getAllReceiptForSpecificUser(res, userId);
             }
             catch (e) {
-                console.log('object creation failed');
                 console.error(e);
             }
         }));
-        // // ROUTES FOR FRIENDS
-        // router.get("/app/friendRequest", async (req, res) => {
-        //   console.log('Query All Friend Requests');
-        //   await this.FriendRequest.retrieveAllFriendRequests(res);
-        // });
-        // router.get("/app/friendRequest/:id", async (req, res) => {
-        //   console.log('Get Friend Request By ID');
-        //   const id = req.params.id;
-        //   await this.FriendRequest.retrieveSpecificFriendRequest(res, id);
-        // });
-        // router.post("/app/friendRequest", async (req, res) => {
-        //   console.log("Create Friend Request");
-        //   const newFriendRequest = req.body;
-        //   const id = crypto.randomBytes(16).toString("hex");
-        //   newFriendRequest.requestID = id;
-        //   const senderId = newFriendRequest.friendRequestSenderID;
-        //   const receiverId = newFriendRequest.friendRequestReceiverID;
-        //   const doc = new this.FriendRequest.model(newFriendRequest);
-        //   try {
-        //     await doc.save();
-        //     this.User.addToFriendRequestReceived(res, receiverId, senderId, newFriendRequest.requestID)
-        //     this.User.addToFriendRequestSent(res, receiverId, senderId, newFriendRequest.requestID)
-        //     res.send('{"id":"' + id + '"}');
-        //   }
-        //   catch (e) {
-        //     console.log('object creation failed');
-        //     console.error(e);
-        //   }
-        // })
-        router.get('/app/receipt', (req, res) => __awaiter(this, void 0, void 0, function* () {
-            console.log("getting all receipt: ");
+        //routes to a single receipt
+        router.get('/app/:userID/receipt/:receiptID', (req, res) => __awaiter(this, void 0, void 0, function* () {
+            const userId = req.params.userID;
+            const receiptID = req.params.receiptID;
+            console.log("getting receipt: ", receiptID, " from user: ", userId);
             try {
-                yield this.Receipt.getAllReceipt(res);
+                const receipt = yield this.Receipt.getSpecificReceipt(res, userId, receiptID);
+                if (receipt) {
+                    res.send(receipt);
+                }
+                else {
+                    res.status(404).json("This user does not have that receipt.");
+                }
             }
             catch (e) {
-                console.log(e);
-                throw e;
+                console.error(e);
+            }
+        }));
+        //routes to post a new receipt 
+        router.post('/app/:userID/receipt', (req, res) => __awaiter(this, void 0, void 0, function* () {
+            const newReceiptId = crypto.randomBytes(16).toString("hex");
+            const userID = req.params.userID;
+            console.log("userID", userID);
+            var receiptObject = req.body;
+            receiptObject.receiptID = newReceiptId;
+            try {
+                const addedReceipt = yield this.Receipt.addSpecificReceipt(receiptObject, userID);
+                res.send(addedReceipt);
+            }
+            catch (e) {
+                console.error(e);
+            }
+        }));
+        //routes to post a new receipt item
+        router.post('/app/:userID/receipt/:receiptID/receiptItem', (req, res) => __awaiter(this, void 0, void 0, function* () {
+            const newReceiptItemId = crypto.randomBytes(16).toString("hex");
+            const receiptID = req.params.receiptID;
+            const userId = req.params.userID;
+            var receiptItemObject = req.body;
+            receiptItemObject.receiptID = receiptID;
+            receiptItemObject.receiptItemID = newReceiptItemId;
+            receiptItemObject.userID = userId;
+            try {
+                const addedReceiptItem = yield this.ReceiptItem.addReceiptItem(receiptItemObject, userId, receiptID);
+                res.send(addedReceiptItem);
+            }
+            catch (e) {
+                console.error(e);
             }
         }));
         this.expressApp.use('/', router);
         this.expressApp.use('/app/json/', express.static(__dirname + '/app/json'));
         this.expressApp.use('/images', express.static(__dirname + '/img'));
         this.expressApp.use('/', express.static(__dirname + '/angularDist/fair-share-angular/browser'));
-        // this.expressApp.use('/', express.static(__dirname + '/pages'));
     }
 }
 exports.App = App;
+// ROUTES FOR USER
+// router.get("/app/user", async (req, res) => {
+//   console.log('Query All User');
+//   await this.User.retreiveAllUsers(res);
+// })
+// router.get("/app/user/:id", async (req, res) => {
+//   console.log("Query Single User");
+//   const id = req.params.id;
+//   await this.User.retreiveSpecificUser(res, id);
+// })
+// router.post('/app/user/', async (req, res) => {
+//   console.log("Adding a user");
+//   const id = crypto.randomBytes(16).toString("hex");
+//   console.log(req.body);
+//   var jsonObj = req.body;
+//   jsonObj.userID = id;
+//   const doc = new this.User.model(jsonObj);
+//   try {
+//     await doc.save();
+//     res.send('{"id":"' + id + '"}');
+//   }
+//   catch (e) {
+//     console.log('object creation failed');
+//     console.error(e);
+//   }
+// });
+// // ROUTES FOR FRIENDS
+// router.get("/app/friendRequest", async (req, res) => {
+//   console.log('Query All Friend Requests');
+//   await this.FriendRequest.retrieveAllFriendRequests(res);
+// });
+// router.get("/app/friendRequest/:id", async (req, res) => {
+//   console.log('Get Friend Request By ID');
+//   const id = req.params.id;
+//   await this.FriendRequest.retrieveSpecificFriendRequest(res, id);
+// });
+// router.post("/app/friendRequest", async (req, res) => {
+//   console.log("Create Friend Request");
+//   const newFriendRequest = req.body;
+//   const id = crypto.randomBytes(16).toString("hex");
+//   newFriendRequest.requestID = id;
+//   const senderId = newFriendRequest.friendRequestSenderID;
+//   const receiverId = newFriendRequest.friendRequestReceiverID;
+//   const doc = new this.FriendRequest.model(newFriendRequest);
+//   try {
+//     await doc.save();
+//     this.User.addToFriendRequestReceived(res, receiverId, senderId, newFriendRequest.requestID)
+//     this.User.addToFriendRequestSent(res, receiverId, senderId, newFriendRequest.requestID)
+//     res.send('{"id":"' + id + '"}');
+//   }
+//   catch (e) {
+//     console.log('object creation failed');
+//     console.error(e);
+//   }
+// })
 //# sourceMappingURL=App.js.map
