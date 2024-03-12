1237226628a388a43eb3788b9f737810795780cc
VengLeapChan <leapvchan@gmail.com>
2024-03-11 23:07:58 -0700

    commit

--- test/get_single_receipt.js
+++ test/get_single_receipt.js
@@ -12,6 +12,14 @@ describe('Test to get specific receipt for a user', function () {
     var response;
     const userID = "100";
     const receiptID = "1";
+    const expectedObject = {
+        _id: '65e96e5e244ab50dc100def1',
+        receiptID: '1',
+        receiptName: 'Grocery Store',
+        receiptTotalAmount: 12,
+        date: '2024-03-07T07:35:58.989Z',
+        userID: '100',
+      };
 
     before(function (done) {
         chai.request(server)
@@ -41,7 +49,14 @@ describe('Test to get specific receipt for a user', function () {
         );
     });
 
-    //check individual value 
+    it('Should return a specific receipt item with specific values', function () {
+        expect(response.body.receiptID).to.equal(expectedObject.receiptID);
+        expect(response.body.receiptName).to.equal(expectedObject.receiptName);
+        expect(response.body.receiptTotalAmount).to.equal(expectedObject.receiptTotalAmount);
+        expect(response.body.userID).to.equal(expectedObject.userID);
+        expect(response.body.date).to.equal(expectedObject.date);
+    });
+
 
 
 });
